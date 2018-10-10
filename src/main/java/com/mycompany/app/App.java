d6622b613683427b77a816c05498feaf1ca71653
root <eelke.hoffman@student.hu.nl>
2018-10-10 12:42:01 -0700

    fout weg gehaald

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,13 +5,12 @@ package com.mycompany.app;
  */
 public class App
 {
-private static  int getal = 4;
     private static final String message = "Hello World!";
 	
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(getal , message);
+        System.out.println(message);
     
 
 
